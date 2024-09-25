2120040c006cad89b722d02df2744853e56b1eb0
AugustoMizu <augusto10barros@gmail.com>
2024-09-24 21:20:00 -0300

    crud em php

---
+++ crud_PHP/CRUD_pdo.php
@@ -0,0 +1,13 @@
+
+<?php
+// criando conexão
+$pdo = new PDO("mysql:dbname=test;host=localhost:3306","root","cimatec");
+
+/*
+if($pdo){
+    echo "conectado";
+}else{
+    echo "não conectado";
+}*/
+// os arquivos php são craidos para o crud como classes de java, sendo chamadados com require e 
+?>
